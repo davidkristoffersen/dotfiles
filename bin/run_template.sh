9abb127b72e3e6421995a9b736e86a20d0cb8bda
David Kristoffersen <david.alta2010@gmail.com>
2020-01-22 04:44:52 +0100

    Restructure and better abstraction of key extraction

--- bin/run_template.sh
+++ bin/run_template.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
 # Source inner template
-. "$HOME/.local/lib/bash/run_template_inner.sh"
+function lib_init() {
+	local lib="$HOME/.local/lib/bash/run_template_inner.sh"
+	if [ ! -f "$lib" ]; then
+		echo "Library not found: $lib" >&2
+		exit
+	fi
+	. $lib
+}
 
-# Create initial variables
-help_init "Example title text"
+function args_init() {
+	# Create initial variables
+	help_init "Example title text"
 
-# Add option
-add_option -s d -m data -i "Input text\nCan be multiline" -d "text"
-add_option -s b -m bool -i "Make bool true"
+	# Add option
+	add_option -s d -m data -i "Input text\nCan be multiline" -d "text"
+	add_option -s b -m bool -i "Make bool true"
+}
 
+# Source library
+lib_init
+# Set argument options
+args_init
 # Parse options
 parse "$@"
 
 # Script
 if key_exists data; then
-	echo "${args[data]}"
+	echo "Data: $(get_key data)"
+else
+	echo "Data not found!"
 fi
