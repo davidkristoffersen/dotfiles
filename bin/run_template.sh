3b51448f0f743cd994cb0189a26fb8e648195d1d
David Kristoffersen <david.alta2010@gmail.com>
2020-01-24 12:02:08 +0100

    Restructure

--- bin/run_template.sh
+++ bin/run_template.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 
-# Source inner template
-function lib_init() {
-	local lib="$HOME/.local/lib/bash/run_template_inner.sh"
-	if [ ! -f "$lib" ]; then
-		echo "Library not found: $lib" >&2
-		exit
-	fi
-	. $lib
+function script() {
+# Script
+if key_exists data; then
+	echo "Data: $(get_key data)"
+fi
 }
 
-function args_init() {
+function lib_args() {
 	# Create initial variables
 	help_init "Example title text"
 
 	# Add option
-	add_option -s d -m data -i "Input text\nCan be multiline" -d "text"
-	add_option -s b -m bool -i "Make bool true"
+	add_option -s a -m alpha -i "ALSO VERY MUCH Make bool true Long text is incoming on this line you should be prepared yes indeed oh man this is scary ho nn"
+	add_option -s b -m bravo -i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon"
+	add_option -s c -m charlie -i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon anvm aga muva nuvag vuag  vv vvmv gmvulv uqgmvu q  vugmuv luavgmln uvagm a vgmula uvgm avuvmg lmvu vugmluvgmlv uamvlu amlamuvla uvgmlav mgaluvamuv lamuvgla uvgmalv avlgmauvl gmauvl agmuv amuvg auvmlgaluvgmal uvglaguvvau "
+	add_option -s d -m delta -i "Input text\nCan be multiline" -d "text"
+	add_option -s e -m echo -i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soo"
 }
 
-# Source library
-lib_init
+#
+# TEMPLATE LIBRARY INIT
+#
+
+# Source template library
+lib="$HOME/.local/lib/bash/run_template_inner.sh"
+if [ ! -f "$lib" ]; then echo "Library not found: $lib" >&2; exit; fi
+. $lib
 # Set argument options
-args_init
+lib_args
 # Parse options
 parse "$@"
-
-# Script
-if key_exists data; then
-	echo "Data: $(get_key data)"
-else
-	echo "Data not found!"
-fi
+# Run script
+script
