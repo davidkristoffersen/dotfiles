58633d1031f14f3baafebed695d116cecb49e400
David Kristoffersen <david.alta2010@gmail.com>
2020-01-27 07:20:56 +0100

    Mode edgecases

--- bin/run_template_dev.sh
+++ bin/run_template_dev.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 function script() {
+	set_key data
 	if key_exists data; then
 		echo "Data: $(get_key data)"
 	fi
@@ -10,14 +11,15 @@ function lib_args() {
 	# Create initial variables
 	help_init "Example title text"
 
-	# Add option
-	add_option -s a -m alpha -i "ALSO VERY MUCH Make bool true Long text is incoming on this line you should be prepared yes indeed oh man this is scary ho nn" -v "number" -d "54"
-	add_option -s b -m bravo -i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon"
-	add_option -m charlie -i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon anvm aga muva nuvag vuag  vv vvmv gmvulv uqgmvu q  vugmuv luavgmln uvagma lwamuv awmlva vlawa vmlawvavu mlauvwavgmula uvgm avuvmg lmvu vugmluvgmlv uamvlu amlamuvla uvgmlavmgaluvamuv lamuvgla uvgmalv avlgmauvl gmauvl agmuv amuvg auvmlgaluvgmal uvglaguvvau nua mgn gmn gm gmnauuv vu mvlua uvlamvu avlamv alvmwav lamuva"
-	add_option -s d -m delta -i "Input text\nCan be multiline" -v "text"
-	add_option -s e -m echo -i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soo" -v "character" -d "n"
-	add_option -s n -m november -i "on this line yell though it is soon"
-	add_option -s q -m quit -i "on this line yeis soon"
+	# Add optio
+	add_option -s a -m alpha	-d "54"		-v "number"	-i "ALSO VERY MUCH Make bool true Long text is incoming on this line you should be prepared yes indeed oh man this is scary ho nn"
+	add_option -s b -m bravo	-d "true"				-i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon"
+	add_option -m charlie								-i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon anvm aga muva nuvag vuag  vv vvmv gmvulv uqgmvu q  vugmuv luavgmln uvagma lwamuv awmlva vlawa vmlawvavu mlauvwavgmula uvgm avuvmg lmvu vugmluvgmlv uamvlu amlamuvla uvgmlavmgaluvamuv lamuvgla uvgmalv avlgmauvl gmauvl agmuv amuvg auvmlgaluvgmal uvglaguvvau nua mgn gmn gm gmnauuv vu mvlua uvlamvu avlamv alvmwav lamuva"
+	add_option -s d -m delta				-v "text"	-i "Input text\nCan be multiline"
+	add_option -s e -m echos	-d	"w"		-v "char"	-i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soo"
+	add_option -s n -m november	-d "false"				-i "on this line yell though it is soon"
+	add_option -s s -m sierra	-d "1"					-i "on this line yeis soon"
+	add_option -s q -m quit		-d "0"					-i "on this line yeis soon"
 }
 
 #
