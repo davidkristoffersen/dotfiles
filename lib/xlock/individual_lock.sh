99730770b628fe398d4b25562f6835fd62e3bacb
David Kristoffersen <david.alta2010@gmail.com>
2020-01-27 10:07:57 +0100

    Working with nitrogen

--- lib/xlock/individual_lock.sh
+++ lib/xlock/individual_lock.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
-xdpyinfo -ext XINERAMA | sed '/^  head #/!d;s///' |
+#!/usr/bin/env bash
+debug=false
+info="$(xdpyinfo -ext all 2>/dev/null | sed '/^  head #/!d;s///')"
+
 while IFS=' :x@,' read i w h x y; do
+	if [ "$i" == 0 ]; then
+		declare -gA main_info=([w]=$w [h]=$h [x]=$x [y]=$y)
+	elif [ "$i" == 1 ]; then
+		declare -gA extended_info=([w]=$w [h]=$h [x]=$x [y]=$y)
+	fi
     import -window root -crop ${w}x$h+$x+$y "/tmp/head_$i.png"
-done
+    if $debug; then echo "import -window root -crop ${w}x$h+$x+$y \"/tmp/head_$i.png\""; fi
+done <<< "$info"
