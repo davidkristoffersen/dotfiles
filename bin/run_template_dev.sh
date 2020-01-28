735042295c0a7163120dd9610c3018e73bd4a465
David Kristoffersen <david.alta2010@gmail.com>
2020-01-28 10:55:25 +0100

    Demo of subcommand

--- bin/run_template_dev.sh
+++ bin/run_template_dev.sh
@@ -5,6 +5,12 @@ function script() {
 	if key_exists data; then
 		echo "Data: $(get_key data)"
 	fi
+
+	if subcmd_exists cache; then
+		if key_exists new; then
+			echo "New: $(get_key new)"
+		fi
+	fi
 }
 
 function lib_args() {
@@ -14,12 +20,16 @@ function lib_args() {
 	# Add optio
 	add_option -s a -m alpha	-d "54"		-v "number"	-i "ALSO VERY MUCH Make bool true Long text is incoming on this line you should be prepared yes indeed oh man this is scary ho nn"
 	add_option -s b -m bravo	-d "true"				-i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon"
-	add_option -m charlie								-i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon anvm aga muva nuvag vuag  vv vvmv gmvulv uqgmvu q  vugmuv luavgmln uvagma lwamuv awmlva vlawa vmlawvavu mlauvwavgmula uvgm avuvmg lmvu vugmluvgmlv uamvlu amlamuvla uvgmlavmgaluvamuv lamuvgla uvgmalv avlgmauvl gmauvl agmuv amuvg auvmlgaluvgmal uvglaguvvau nua mgn gmn gm gmnauuv vu mvlua uvlamvu avlamv alvmwav lamuva"
+	add_option 		-m charlie							-i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soon anvm aga muva nuvag vuag  vv vvmv gmvulv uqgmvu q  vugmuv luavgmln uvagma lwamuv awmlva vlawa vmlawvavu mlauvwavgmula uvgm avuvmg lmvu vugmluvgmlv uamvlu amlamuvla uvgmlavmgaluvamuv lamuvgla uvgmalv avlgmauvl gmauvl agmuv amuvg auvmlgaluvgmal uvglaguvvau nua mgn gmn gm gmnauuv vu mvlua uvlamvu avlamv alvmwav lamuva"
 	add_option -s d -m delta				-v "text"	-i "Input text\nCan be multiline"
 	add_option -s e -m echos	-d	"w"		-v "char"	-i "Make bool true Long text is incoming on this line you should be prepared too as well though it is soo"
 	add_option -s n -m november	-d "false"				-i "on this line yell though it is soon"
 	add_option -s s -m sierra	-d "1"					-i "on this line yeis soon"
 	add_option -s q -m quit		-d "0"					-i "on this line yeis soon"
+
+	add_subcmd cache "Cache subcmd title"
+	add_option -s n -m new		-d "34"		-v "number"	-i "This is a subcmd option" --subcmd "cache"
+	add_option -s x -m xtra		-d "true"				-i "This is another subcmd option" --subcmd "cache"
 }
 
 #
