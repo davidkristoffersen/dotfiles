7aaa95417c6fe1810700eeb5bbce235cbad9eb13
David Kristoffersen <david.alta2010@gmail.com>
2020-02-09 00:18:05 +0100

    wip

--- lib/bash/run/config.py
+++ lib/bash/run/config.py
@@ -1,3 +1,5 @@
+from dotmap import DotMap
+
 def _default_text(text='Example text', col='white'):
     return {
         'text': text,
@@ -5,66 +7,67 @@ def _default_text(text='Example text', col='white'):
     }
 
 default_title = {
-    'header': _default_text(col='cyan'),
+    'header': _default_text(text='MAIN', col='cyan'),
     'body': _default_text(),
-    'sep': ': '
+    'sep': _default_text(text=': ')
 }
 
 default_usage = {
-    'header': _default_text(),
+    'header': _default_text(text='usage'),
     'body': _default_text(),
-    'sep': ' '
+    'sep': _default_text(text=' ')
 }
 
 default_usages = {
-    'header': _default_text(col='yellow'),
+    'header': _default_text(text='USAGE', col='yellow'),
     'body': [],
-    'sep': ':\n'
+    'sep': _default_text(text=':\t')
 }
 
 default_option = {
-    'header': _default_text(),
+    'header': _default_text(text='option', col='greel'),
     'single': _default_text(col='green'),
     'multi': _default_text(col='green'),
     'value': _default_text(col='green'),
     'default': _default_text(col='magenta'),
     'body': _default_text(),
-    'sep': '\t'
+    'sep': _default_text(text='\t')
 }
 
 default_options = {
-    'header': _default_text(col='yellow'),
+    'header': _default_text(text='OPTIONS', col='yellow'),
     'body': {},
-    'sep': ':\n'
+    'sep': _default_text(text=':')
 }
 
 default_arg = {
-    'header': _default_text(col='green'),
+    'header': _default_text(text='arg', col='green'),
     'body': _default_text(),
-    'sep': '\t'
+    'sep': _default_text(text='\t')
 }
 
 default_args = {
-    'header': _default_text(col='yellow'),
+    'header': _default_text(text='ARGS', col='yellow'),
     'body': {},
-    'sep': ':\n',
+    'sep': _default_text(text=':')
 }
 
 default_subcmd = {
-    'header': _default_text(col='green'),
+    'header': _default_text(text='subcmd', col='green'),
     'body': _default_text(),
-    'sep': '\t'
+    'sep': _default_text(text='\t')
 }
 
 default_subcmds = {
-    'header': _default_text(col='yellow'),
+    'header': _default_text(text='SUBCOMMANDS', col='yellow'),
     'body': {},
-    'sep': ':\n',
+    'cmd': {},
+    'sep': _default_text(text=':')
 }
 
 default_json = {
     'title': default_title,
-    'usage': default_usage,
+    'usages': default_usages,
     'options': default_options,
     'args': default_args,
     'subcmds': default_subcmds
--- lib/bash/run/run.py
+++ lib/bash/run/run.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
 from json import load as fload, dump as fdump, loads as oload,  dumps as odump
-import sys
+import sys, os
 from pprint import pprint
 from dotmap import DotMap
 from config import *
 from colorclass import Color
 
+def gen_default_type(t):
+    return DotMap({
+        'u': default_usage,
+        'o': default_option,
+        'a': default_arg,
+        's': default_subcmd,
+    }[t])
+
 def gen_default_json():
-    return DotMap(default_json)
+    json = DotMap(default_json)
+
+    json.options.body.help = gen_default_type('o')
+    json.options.body.help.header.text = 'help'
+    json.options.body.help.body.text = 'Print this help menu.'
+
+    json = DotMap(json)
+    return json
 
 def read(name):
     try:
@@ -20,37 +35,78 @@ def read(name):
 
 def modify(json):
     if not json:
-        json = gen_default_json()
-        json.title.header = "MAIN"
-        return json
+        return gen_default_json()
 
     json = DotMap(json)
 
+    json.args.body.file = gen_default_type('a')
+
     cache = gen_default_json()
-    cache.title.header = "CACHE"
+    cache.title.header.text = "CACHE"
+    json.subcmds.body.cache = gen_default_type('s')
+    json.subcmds.cmd.cache = cache
+
     config = gen_default_json()
-    config.title.header = "CONFIG"
+    config.title.header.text = "CONFIG"
+    json.subcmds.body.config = gen_default_type('s')
+    json.subcmds.cmd.config = config
+
     cache_config = gen_default_json()
-    cache_config.title.header = "CACHE_CONFIG"
+    cache_config.title.header.text = "CACHE_CONFIG"
+    json.subcmds.cmd.cache.subcmds.body.cache_config = gen_default_type('s')
+    json.subcmds.cmd.cache.subcmds.cmd.cache_config = cache_config
 
-    json.subcmds.cmds.cache = cache
-    json.subcmds.cmds.config = config
-    json.subcmds.cmds.cache.subcmds.cmds.cache_config = cache_config
-
+    json = DotMap(json)
     return json
 
 def print_json(json, rec=0):
     num_tabs = '\t' * rec
-    for key, val in json.items():
-        print('\n' + num_tabs + key + '\n' + num_tabs + val.header + val.sep + str(val.body))
+    _num_tabs = '\t' * (rec + 1)
 
-    for subcmd in json.subcmds.cmds.values():
+    t = json.title
+    u = json.usages
+    o = json.options
+    a = json.args
+    s = json.subcmds
+
+    # Title
+    print()
+    print(num_tabs + t.header.text + t.sep.text + t.body.text)
+    print()
+
+    # Usages
+    print(num_tabs + u.header.text + u.sep.text)
+    for _u in u.body:
+        print(num_tabs + _u.header.text + _u.sep.text + _u.body.text)
+    print()
+
+    # Options
+    print(num_tabs + o.header.text + o.sep.text)
+    for _o in o.body.values():
+        print(_num_tabs + _o.header.text + _o.sep.text + _o.body.text)
+    print()
+
+    # args
+    print(num_tabs + a.header.text + a.sep.text)
+    for _a in a.body.values():
+        print(_num_tabs + _a.header.text + _a.sep.text + _a.body.text)
+    print()
+
+    # args
+    print(num_tabs + s.header.text + s.sep.text)
+    for _s in s.body.values():
+        print(_num_tabs + _s.header.text + _s.sep.text + _s.body.text)
+    print()
+
+    # Subcmds
+    for subcmd in s.cmd.values():
         print_json(subcmd, rec=rec+1)
     print()
 
 def write(name, json):
     with open(name, 'w') as f:
         fdump(json, f)
+    os.system('cat ' + name + ' | jq --tab | sponge ' + name)
 
 if __name__ == "__main__":
     name = "test.json"
