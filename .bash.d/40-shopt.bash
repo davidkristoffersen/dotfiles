720e8f0066b5ac852adb168b839bb90ff3f84f31
David Kristoffersen <david.alta2010@gmail.com>
2020-02-14 10:58:45 +0100

    Restructure wip

---
+++ .bash.d/40-shopt.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set number of spaces in tabs
+if $SHELL_INTERACTIVE; then
+	tabs 4
+fi
+
+# Don't put duplicate lines or lines starting with space in the history.
+HISTCONTROL=ignoreboth
+
+# Append to the history file, don't overwrite it
+shopt -s histappend
+
+# For setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+# Check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+shopt -s globstar
+
+# Make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# Enable programmable completion features
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
---
+++ .bash.d/60-aliases.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Navigation
+# enable color support of ls and also add handy aliases
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias tree='tree -C'
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias srcbash="source $HOME/.bashrc"
+alias bashrc="vim $HOME/.bashrc"
+alias bashconfig="vim -p `echo $HOME/.bash* | xargs -n 1 | grep -ve ".bash_history\|.bash_logout" | xargs`"
+alias vimrc='vim $MYVIMRC'
+alias vim_plugins="cd $HOME/.config/config/.vim/bundle"
+alias config="cd $HOME/.config/config"
+
+alias c='clear'
+
+# Program specific
+alias rip='gio trash'
+alias lock='i3lock-fancy -pf inconsolata'
+alias python='python3'
+alias pcloud_start='pcloud > /dev/null 2>&1 &'
+alias vless="$PAGER"
