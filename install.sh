6e5b8d1dc3e80ba1d87dc2c15ee6be0bef57a592
David Kristoffersen <david.alta2010@gmail.com>
2020-02-14 19:17:03 +0100

    All config files in root

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ function link_file() {
 	local desc_long="Replacing $3:\t"
 	local desc_len="${#desc_long}"
 	local desc_long="${BLUE}$desc_long${RESET}\n"
+	local dirs="$(dirname "$dst")"
 
 	printf "$desc_long"
+	printf "\tmkdir -p \"$dirs\"\n"; check_error $?
 	printf "\trm -rf \"$dst\"\n"; check_error $?
 	printf "\tln -s \"$src\" \"$dst\"\n"; check_error $?
 	print_at 3 $OK_POS "${GREEN}OK${RESET}"
@@ -27,27 +29,46 @@ home_dot_files=(.bash_profile
 				.bashrc.d
 				.bash_logout
 				.gitconfig
+				.tmux.conf
 )
 
 for home_dot_file in ${home_dot_files[@]}; do
 	link_file $home_dot_file $home_dot_file "$home_dot_file"
 done
+echo
 
-# Vimrc files
-link_file .vim .vim "vim dir config"
-link_file .vimrc .vim/.vimrc "vim config"
-
+# Session management
 # I3
 link_file i3.config .config/i3/config "i3 config"
 
 # Rofi
 link_file rofi.rasi .config/rofi/config.rasi "rofi config"
 
+# Terminator
+link_file terminator.ini .config/config/terminator.ini "terminator config"
+echo
+
+# Editor
+# Vimrc files
+link_file .vim .vim "vim dir config"
+link_file .vimrc .vim/.vimrc "vim config"
+
+# Latex
+link_file template.latex .config/latex/template.latex "Latex template"
+echo
+
+# CLI programs
 # SQLite
 link_file .sqliterc.sql .sqliterc "SQLite config"
 
-# Terminator
-link_file terminator.ini .config/config/terminator.ini "terminator config"
+# Htop
+link_file htoprc .config/htop/htoprc "htop config"
+echo
 
+# GUI programs
 # GIMP
 link_file gimprc .gimp-2.0/gimprc "GIMP config"
+
+# Gitk
+link_file gitk .config/git/gitk "gitk config"
+echo
