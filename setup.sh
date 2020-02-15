5c71e7bd81b02f745dc851c12cdcaa4c63df906c
David Kristoffersen <david.alta2010@gmail.com>
2020-02-16 00:19:16 +0100

    First run of setup and error check

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-# Setup installation of config files
+# Setup installation of dotfiles
 
-config="$HOME/.config/config"
+dotfiles="$HOME/dotfiles"
 
 # Clone the git repository
-printf "git clone https://github.com/davidkristoffersen/config.git $config\n"
+printf "git clone https://github.com/davidkristoffersen/dotfiles.git $dotfiles\n"
+git clone https://github.com/davidkristoffersen/dotfiles.git $dotfiles
+[ $? -ne 0 ] && exit
 
-printf "cd $config\n"
+printf "cd $dotfiles\n"
+cd $dotfiles
+[ $? -ne 0 ] && exit
+
 printf "./install.sh\n"
+./install.sh
