bcb6cf2e24c34ee3261481cc09daa0dbf807891b
David Kristoffersen <david.alta2010@gmail.com>
2020-02-29 22:29:51 +0100

    Package installer

---
+++ src/pacman.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+dotfiles_pacman() {
+	# AUR helper
+	print_section "yay"
+	sudo pacman -S yay
+
+	# AUR packages
+	print_section "AUR"
+	pacman_manager.py foreign print
+	echo
+	local _pkgs="$(pacman_manager.py foreign)"
+	echo "yay -S $_pkgs"
+
+	# Native packages
+	print_section "Native" "\n"
+	# pacman_manager.py native print
+	echo
+	_pkgs="$(pacman_manager.py native)"
+	echo "sudo pacman -S $_pkgs"
+}
+
+. $DOTFILES_SRC/init.sh dotfiles_pacman
