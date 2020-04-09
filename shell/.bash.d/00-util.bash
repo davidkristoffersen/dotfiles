5689e6fa8855cd5d9716d16464a9df2c5df20682
David Kristoffersen <david.alta2010@gmail.com>
2020-04-10 01:20:02 +0200

    Improved check error

--- shell/.bash.d/00-util.bash
+++ shell/.bash.d/00-util.bash
@@ -4,12 +4,22 @@ confirm() {
 	printf "Do you want to execute this cmd? (y/n) "
 }
 
+check_error_help() {
+	printf "Usage: check_error error_code [OPTIONS] [DESCRIPTION]\n"
+	printf "\tnargs:\tInvalid number of arguments\n"
+	printf "\targ:\tInvalid argument (arg)\n"
+	printf "\targv:\tInvalid argument value (arg value)\n"
+	printf "\tfile:\tError in (file)\n"
+	printf "\tline:\tLine number (line)\n"
+	printf "\thelp:\tPrint this help message.\n"
+	exit $error
+}
+
 check_error() {
 	if [ "$1" -ne "0" ]; then
 		local error="$1"
 		shift
 		local desc="\tFile: ${BASH_SOURCE[1]}\n\tFunc: ${FUNCNAME[1]}"
-		echo $@, $2
 		while (( "$#" )); do
 			case "$1" in
 				nargs)
@@ -19,6 +29,21 @@ check_error() {
 				arg)
 					[ ${#@} -gt 1 ] && local arg="$2" && shift || local arg="(Missing argument)"
 					desc+="\n\tDesc: Invalid argument $arg"
+					shift
+					;;
+				argv)
+					if [ ${#@} -gt 2 ]; then
+						local arg="$2" && local val="$3"
+						shift && shift
+					elif [ ${#@} -eq 2 ]; then
+						local arg="$2"
+						shift
+						local val="(Missing value)"
+					else
+						local arg="(Missing argument)"
+						local val="(Missing value)"
+					fi
+					desc+="\n\tDesc: Invalid argument value $arg=$val"
 					shift
 					;;
 				file)
@@ -31,6 +56,9 @@ check_error() {
 					desc+="\n\tLine: $arg"
 					shift
 					;;
+				help)
+					check_error_help $error
+					;;
 				*)
 					[ ${#@} -ne 0 ] && desc+="\n\tDesc: $@"
 					break
@@ -42,4 +70,4 @@ check_error() {
 	fi
 }
 
-export -f check_error confirm
+export -f check_error confirm check_error_help
