22ac8bc3b0b21e2be084f873c48a639e676d6819
David Kristoffersen <david.alta2010@gmail.com>
2020-10-28 15:17:03 +0100

    Fix history in fuzzy open files

--- shell/.bash.d/85-keybindings.bash
+++ shell/.bash.d/85-keybindings.bash
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
 if $SHELL_INTERACTIVE; then
+	_fzf_open_single='_file="$(fzf -m --height 60%)" && history -s "vim $_file" && vim $_file'
+	_fzf_open_multi='_files="$(fzf -m --height 60%)" && history -s "vim -p $(xargs <<< "$_files")" && vim -p $_files'
+
 	# Fuzzy open one file in vim
-	bind -m vi-command '"\C-f": "_file=\"$(fzf --height 60%)\" && vim $_file\C-m"'
-	bind -m vi-insert '"\C-f": "_file=\"$(fzf --height 60%)\" && vim $_file\C-m"'
-	# Fuzzy open one multiple files in vim
-	bind -m vi-command '"\C-n": "_files=\"$(fzf -m --height 60%)\" && vim -p $_files\C-m"'
-	bind -m vi-insert '"\C-n": "_files=\"$(fzf -m --height 60%)\" && vim -p $_files\C-m"'
+	bind -m vi-command "'\C-f: '$_fzf_open_single\C-m'"
+	bind -m vi-insert "'\C-f: '$_fzf_open_single\C-m'"
+	# Fuzzy open multiple files in vim
+	bind -m vi-command "'\C-n: '$_fzf_open_multi\C-m'"
+	bind -m vi-insert "'\C-n: '$_fzf_open_multi\C-m'"
 fi
