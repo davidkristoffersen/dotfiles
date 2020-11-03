b875953a866bdb693dc58c09d44e0c8387a40433
David Kristoffersen <david.alta2010@gmail.com>
2020-11-03 14:04:39 +0100

    Fix keybinds in vi command mode

--- shell/.bash.d/85-keybindings.bash
+++ shell/.bash.d/85-keybindings.bash
@@ -1,13 +1,13 @@
-#'/usr/bin/bash
+#!/usr/bin/bash
 
 if $SHELL_INTERACTIVE; then
 	# Fuzzy cd upwards cwd
-	bind -m vi-command '"\eC": " fzf_upwards_pwd\C-m"'
+	bind -m vi-command '"\eC": "i fzf_upwards_pwd\C-m\e"'
 	bind -m vi-insert '"\eC": " fzf_upwards_pwd\C-m"'
 	# Fuzzy open one file in vim
-	bind -m vi-command '"\C-f": " fzf_open_single\C-m"'
+	bind -m vi-command '"\C-f": "i fzf_open_single\C-m\e"'
 	bind -m vi-insert '"\C-f": " fzf_open_single\C-m"'
 	# Fuzzy open multiple files in vim
-	bind -m vi-command '"\C-n": " fzf_open_multi\C-m"'
+	bind -m vi-command '"\C-n": "i fzf_open_multi\C-m\e"'
 	bind -m vi-insert '"\C-n": " fzf_open_multi\C-m"'
 fi
