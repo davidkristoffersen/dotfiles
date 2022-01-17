a71b21e4e2d198e242a8ace7869da9a0c9c481b1
davidkristoffersen <david.alta2010@gmail.com>
2022-01-17 17:36:55 +0100

    Remove old install scripts

--- src/old/config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-dotfiles_config() {
-	SUDO=true
-	PRINT=true
-	WRITE=true
-	SUBMODULE=true
-}
-dotfiles_config
--- src/old/fini.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-dotfiles_fini() {
-	sudo -K
-	cd "$pwd_org"
-}
-dotfiles_fini
