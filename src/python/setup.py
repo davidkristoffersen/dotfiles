c80a8736ef122b9d14ba219d24286cc38b514a67
davidkristoffersen <david.alta2010@gmail.com>
2022-01-17 17:27:44 +0100

    Fix flags order

--- src/python/setup.py
+++ src/python/setup.py
@@ -12,19 +12,19 @@ def parse_args() -> argparse.ArgumentParser:
     parser.add_argument(
         '-s', '--script', choices=['meta', 'shell', 'home', 'bin', 'lib', 'share',  'repo', 'private'], help='Script name to install')
     parser.add_argument(
-        '-a,', '--apt', dest='apt', action='store_true', help='Run apt install')
-    parser.add_argument(
-        '--pacman', dest='pacman', action='store_true', help='Run pacman install')
-    parser.add_argument(
-        '--sub', dest='sub', action='store_true', help='Initialize submodules')
-    parser.add_argument(
         '-w', '--write', dest='write', action='store_true', help='Do write operations')
     parser.add_argument(
         '-l', '--log', choices=['all', 'trace', 'debug', 'info', 'category', 'warn', 'error', 'fatal', 'off'], help='Set log level')
     parser.add_argument(
-        '--server', dest='server', action='store_true', help='Run server install')
+        '-a', '--apt', dest='apt', action='store_true', help='Run apt install')
     parser.add_argument(
         '-t', '--test', dest='test', action='store_true', help='Run test install')
+    parser.add_argument(
+        '--pacman', dest='pacman', action='store_true', help='Run pacman install')
+    parser.add_argument(
+        '--server', dest='server', action='store_true', help='Run server install')
+    parser.add_argument(
+        '--sub', dest='sub', action='store_true', help='Initialize submodules')
     argcomplete.autocomplete(parser)
     return parser.parse_args()
 
