7967fca8f6d10e6d94a1c54c9836040d9994de10
David Kristoffersen <david.alta2010@gmail.com>
2022-01-24 13:27:45 +0100

    Add all subdirectories of bin to path

--- shell/.profile.d/50-path.bash
+++ shell/.profile.d/50-path.bash
@@ -7,10 +7,21 @@ path_add() {
 	fi
 }
 
+# Recursively add to path, use with caution!
+path_add_rec() {
+	if [ -d "$1" ]; then
+		for _d in $1/*; do
+			path_add "$_d"
+		done
+	fi
+}
+
 # User's bin
 path_add "$HOME/bin"
 # User's private bin
 path_add "$HOME/.local/bin"
+# User's private bin subdirectories
+path_add_rec "$HOME/.local/bin"
 # Cargo's bin
 path_add "$HOME/.cargo/bin"
 # Basher's bin
