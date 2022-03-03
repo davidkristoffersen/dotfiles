242589a53a872cfed09c20d0d6e6901be1ec7433
David Kristoffersen <david.alta2010@gmail.com>
2022-03-03 13:39:07 +0100

    Settings changes

--- home/.config/vscode/update.sh
+++ home/.config/vscode/update.sh
@@ -4,13 +4,20 @@ wslpath_win_dir="$WIN_HOME/AppData/Roaming/Code/User"
 win_dir="$(wslpath -w "$WIN_HOME/AppData/Roaming/Code/User")"
 wsl_dir="$(wslpath -w .)"
 
-update() {
-	cp -f "$1" "$wslpath_win_dir/$1.bak"
-	rm -f "$wslpath_win_dir/$1"
+mklink() {
 	cd $wslpath_win_dir
+	sudo rm -f "$1"
 	cmd.exe /C "mklink $win_dir\\$1 $wsl_dir\\$1 >nul"
 	cd - >/dev/null
 }
 
-update settings.json
-update keybindings.json
+backup() {
+	sudo cp -f "$1" "$wslpath_win_dir/$1.bak"
+}
+
+main() {
+	backup settings.json
+	backup keybindings.json
+}
+
+main
