44c67ea77a8210fa2c31e0b8c70b322c08f3359e
David Kristoffersen <david.alta2010@gmail.com>
2022-03-03 13:32:23 +0100

    Temporarily comment out reduntant wslg fix

--- shell/.profile.d/70-xserver.bash
+++ shell/.profile.d/70-xserver.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
+# TMP
 # Fix for hanging gui apps in wslg
-if $SHELL_WSLG; then
-	export DISPLAY="$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0"
-fi
+# if $SHELL_WSLG; then
+# export DISPLAY="$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0"
+# fi
