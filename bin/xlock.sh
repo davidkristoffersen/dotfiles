885396a6ac56e8fb86cf0f927b92e343475c0880
David Kristoffersen <david.alta2010@gmail.com>
2023-03-10 22:14:11 +0100

    Improve lock script

--- bin/xlock.sh
+++ bin/xlock.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 tmpdir="/tmp/"
-tmpfile="${tmpdir}head_tmp.png"
 overlay_img="$HOME/.local/share/icons/xlock/locked.png"
 
 pre_png="${tmpdir}head_"
@@ -9,115 +8,133 @@ post_png=".png"
 out="${tmpdir}screen.png"
 
 debug=true
-
-get_autorandr_profile() {
-	local profile="$(autorandr_profile.sh)"
-	config="$(autorandr_manage.sh -c $profile)"
-}
-
-json_config() {
-	if [ $# -eq 1 ]; then
-		jq -r '.["'$1'"]' <<<"$config"
-	elif [ $# -eq 2 ]; then
-		jq -r '.["'$1'"]["'$2'"]' <<<"$config"
-	elif [ $3 == "list" ]; then
-		jq -r '.["'$1'"]['$2']' <<<"$config"
-	fi
-}
-
-screenshot() {
-	num_screens="$(json_config num_screens)"
-	for i in $(seq 0 $((num_screens - 1))); do
-		local screen="$(json_config screens $i list)"
-		local out="/tmp/head_$i.png"
-
-		local w="$(json_config $screen w)"
-		local h="$(json_config $screen h)"
-		local x="$(json_config $screen x)"
-		local y="$(json_config $screen y)"
-		$debug && echo -e "$timeit_tab$out:\t${w}x${h}+${x}+${y}"
-
-		scrot -o -a ${x},${y},${w},${h} $out
-	done
-}
-
-modify_par() {
-	. $(which env_parallel.bash)
-	echo $(seq 0 $((num_screens - 1))) | tr " " "\n" | env_parallel timeit modify
-}
-
-modify_seq() {
-	for i in $(seq 0 $((num_screens - 1))); do
-		timeit modify $i
-	done
-}
-
-modify() {
-	blurred $1
-	pixelize $1
-	overlay $1
-}
-
-blurred() {
-	local _png=$pre_png$1$post_png
-	gm convert $_png -scale 10% -blur 0x2.5 -resize 1000% $_png
-}
-
-pixelize() {
-	local _png=$pre_png$1$post_png
-	pixelize.py $_png $_png 15
-}
-
-overlay() {
-	local _file="$pre_png$1$post_png"
-	if [[ -f $overlay_img ]]; then
-		gm composite -matte -gravity center $overlay_img $_file $_file
-	fi
-}
-
-combine() {
-	if [ "$num_screens" == 1 ]; then
-		mv ${pre_png}0$post_png $out
-		return
-	fi
-
-	width="$(json_config max_w)"
-	heigth="$(json_config max_h)"
-	min_x="$(json_config min_x)"
-	min_y="$(json_config min_y)"
-	main="${pre_png}0$post_png"
-	extended=""
-
-	for i in $(seq 1 $((num_screens - 1))); do
-		local screen="$(json_config screens $i list)"
-		local x="$(json_config $screen x)"
-		local y="$(json_config $screen y)"
-		extended+="-page +$x+$y $pre_png$i$post_png "
-	done
-
-	$debug && echo -e "${timeit_tab}convert -page \"${width}x${heigth}+$min_x+$min_y\" $main $extended -flatten $out"
-	convert -page "${width}x${heigth}+$min_x+$min_y" $main $extended -flatten $out
-}
-
-_xlock() {
-	i3lock -u -i $out
-}
+r="\e[0m"
+b="\e[1m"
+f="\e[2m"
 
 main() {
-	# timeit get_autorandr_profile
-	# timeit screenshot
-	# timeit modify_seq
-	# timeit combine
-	timeit _xlock
+	num_screens="$(($(xrandr | grep " connected" | wc -l)))"
+	res="$(xrandr | rg -o --pcre2 "((?<= connected )|(?<= connected primary ))\d[^ ]+" | tr 'x' ' ' | tr '+' ' ')"
+	names="$(xrandr | rg -o --pcre2 "^[^ ]+(?= connected)")"
+	primary="$(xrandr | rg -o --pcre2 "^[^ ]+(?= connected primary)")"
+
+	tagstring='\033[30;3{=$_=++$::color%8=}m'
+	screens="$(echo $(seq 0 $((num_screens - 1))) | tr " " "\n")"
+	mapfile -t arr < <(parallel --tagstring $tagstring seq_main {} "$primary" ::: $screens)
+
+	if [ "$num_screens" -gt 1 ]; then
+		combine
+	else
+		cp "${pre_png}0$post_png" "$out"
+	fi
+
+	xlock
 }
 
-test_func() {
-	test "$(type $1 2>/dev/null)" || eval "$1(){ \$@; }"
+seq_main() {
+	i=$1
+
+	read w h x y <<<$(echo "$res" | sed -n "$((i + 1)) p")
+	read name <<<$(echo "$names" | sed -n "$((i + 1)) p")
+	# Check if current screen is primary
+	[ "$2" == "$name" ] && primary=true || primary=false
+	screen="$pre_png$i$post_png"
+
+	$debug && echo "$name: $w x $h + $x + $y -> $screen" 1>&2
+
+	# screenshot
+	$debug && echo -e "${f}Scrotting...$r" 1>&2
+	scrot -o -a ${x},${y},${w},${h} $screen
+
+	# blur
+	$debug && echo -e "${f}Blurring...$r" 1>&2
+	gm convert $screen -scale 10% -blur 0x2.5 -resize 1000% $screen
+
+	# pixelize
+	$debug && echo -e "${f}Pixelizing...$r" 1>&2
+	pixelize.py $screen $screen 15
+
+	# overlay if not primary
+	if [[ -f $overlay_img ]] && ! $primary; then
+		$debug && echo -e "${f}Overlaying...$r" 1>&2
+		rw=$((w / 5))
+		rh=$((h / 5))
+		gm composite -matte -gravity center -resize ${rw}x$rh $overlay_img $screen $screen
+	fi
+
+	$debug && echo -e "${f}Setting page info...$r" 1>&2
+	pages="-page +$x+$y $screen "
+
+	echo -e "$w|$h|$x|$y|$pages"
+}
+export -f seq_main
+export debug f r res names pre_png post_png overlay_img
+
+combine() {
+	root="${pre_png}0$post_png"
+	tot_w=0
+	tot_h=0
+
+	$debug && echo -e "\n${b}Combining images${r}"
+
+	for ((i = 0; i < num_screens; i++)); do
+		# Change delimiter to '|' and read from arr to w, h, x, y, pages
+		IFS='|' read -e w h x y _pages <<<"$(echo -e "${arr[$i]}" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')"
+
+		# Trim whitespace
+		w="$(echo "$w" | tr -d "[:blank:]")"
+
+		$debug && echo -e "${arr[$i]}$r"
+		$debug && echo -e "${f}w: $w, h: $h, x: $x, y: $y, pages: $_pages$r"
+
+		pages+="$_pages"
+		[[ $((w + x)) -gt $tot_w ]] && tot_w=$((w + x))
+		[[ $((h + y)) -gt $tot_h ]] && tot_h=$((h + y))
+	done
+
+	$debug && echo -e "${f}convert -page \"${tot_w}x${tot_h}+0+0\" $root $pages -flatten $out$r"
+	convert -page "${tot_w}x${tot_h}+0+0" $root $pages -flatten $out
 }
 
-# test_func timeit
-dunstify "${BASH_SOURCE[@]}, $0"
-# dunstify "$(type timeit)"
-# dunstify "$(type set_colors)"
-# env > /tmp/env_
-# timeit main
+xlock() {
+	# i3lock -u -i $out
+	BLANK='#00000000'
+	CLEAR='#ffffff0a'
+	DEFAULT='#f07416cc'
+	TEXT='#c45f12cc'
+	WRONG='#880000bb'
+	VERIFYING='#c45f12bb'
+
+	i3lock \
+		-i $out \
+		-c $BLANK \
+		\
+		--insidever-color=$CLEAR \
+		--ringver-color=$VERIFYING \
+		\
+		--insidewrong-color=$CLEAR \
+		--ringwrong-color=$WRONG \
+		\
+		--inside-color=$BLANK \
+		--ring-color=$DEFAULT \
+		--line-color=$BLANK \
+		--separator-color=$DEFAULT \
+		\
+		--verif-color=$TEXT \
+		--wrong-color=$TEXT \
+		--time-color=$TEXT \
+		--date-color=$TEXT \
+		--layout-color=$TEXT \
+		--keyhl-color=$WRONG \
+		--bshl-color=$WRONG \
+		\
+		--screen 1 \
+		--clock \
+		--indicator \
+		--time-str="%H:%M:%S" \
+		--date-str="%A, %Y-%m-%d" \
+		--keylayout 1
+
+}
+
+main
