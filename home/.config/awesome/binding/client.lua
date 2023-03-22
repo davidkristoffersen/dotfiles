d805e70766c3c884c683b695877426617f734bc3
David Kristoffersen <david.alta2010@gmail.com>
2023-03-22 16:17:15 +0100

    Use key object in client keys

--- home/.config/awesome/binding/client.lua
+++ home/.config/awesome/binding/client.lua
@@ -2,63 +2,49 @@ local awful   = require('awful')
 local gears   = require('gears')
 
 local binding = require('util.binding.init')
-local M, K, B = binding.mod, binding.key, binding.bind
-local key     = binding.funcs.set_key
-
-local modkey  = M.m[1]
+local B, key  = binding.bind, binding.funcs.set_key
 
 
 local keys = gears.table.join(
+-- Toggling
     key(B.m.f, function (c)
         c.fullscreen = not c.fullscreen
         c:raise()
     end, 'toggle fullscreen', 'client'),
-    key(B.ms.c, function (c) c:kill() end, 'close', 'client'),
     key(B.mc.space, awful.client.floating.toggle,
         'toggle floating', 'client'),
+    key(B.ms.c, function (c) c:kill() end, 'close', 'client'),
+    key(B.m.t, function (c) c.ontop = not c.ontop end,
+        'toggle keep on top', 'client'),
+
+    -- Moving
     key(B.mc.return_, function (c) c:swap(awful.client.getmaster()) end,
         'move to master', 'client'),
     key(B.m.o, function (c) c:move_to_screen() end,
         'move to screen', 'client'), -- Move to next/previous screen
-    awful.key(
-        {modkey, 'Mod1'}, 'Right',
-        function (c) c:move_to_screen(c.screen.index + 1) end, {
-            description = 'move to next screen',
-            group = 'client',
-        }
-    ), awful.key(
-        {modkey, 'Mod1'}, 'Left',
-        function (c) c:move_to_screen(c.screen.index - 1) end, {
-            description = 'move to previous screen',
-            group = 'client',
-        }
-    ), awful.key(
-        {modkey}, 't', function (c) c.ontop = not c.ontop end, {
-            description = 'toggle keep on top',
-            group = 'client',
-        }
-    ), awful.key(
-        {modkey}, 'n', function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end, {description = 'minimize', group = 'client'}
-    ), awful.key(
-        {modkey}, 'm', function (c)
-            c.maximized = not c.maximized
-            c:raise()
-        end, {description = '(un)maximize', group = 'client'}
-    ), awful.key(
-        {modkey, 'Control'}, 'm', function (c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end, {description = '(un)maximize vertically', group = 'client'}
-    ), awful.key(
-        {modkey, 'Shift'}, 'm', function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end, {description = '(un)maximize horizontally', group = 'client'}
-    )
+    key(B.ma.right, function (c) c:move_to_screen(c.screen.index + 1) end,
+        'move to next screen', 'client'),
+    key(B.ma.left, function (c) c:move_to_screen(c.screen.index - 1) end,
+        'move to previous screen', 'client'),
+
+    -- Resizing
+    key(B.m.n, function (c)
+        -- The client currently has the input focus, so it cannot be
+        -- minimized, since minimized clients can't have the focus.
+        c.minimized = true
+    end, 'minimize', 'client'),
+    key(B.m.m, function (c)
+        c.maximized = not c.maximized
+        c:raise()
+    end, '(un)maximize', 'client'),
+    key(B.mc.m, function (c)
+        c.maximized_vertical = not c.maximized_vertical
+        c:raise()
+    end, '(un)maximize vertically', 'client'),
+    key(B.ms.m, function (c)
+        c.maximized_horizontal = not c.maximized_horizontal
+        c:raise()
+    end, '(un)maximize horizontally', 'client')
 )
 
 return {
