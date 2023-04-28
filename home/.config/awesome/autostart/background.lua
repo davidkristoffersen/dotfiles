15d33c8c9be995b46aa94fb144c069d59a399988
David Kristoffersen <david.alta2010@gmail.com>
2023-04-28 09:38:51 +0200

    Add setting keyboard layout

--- home/.config/awesome/autostart/background.lua
+++ home/.config/awesome/autostart/background.lua
@@ -4,3 +4,6 @@ local spawn = require('helpers.spawn')
 
 -- Polkit authentication agent
 spawn('/usr/lib/polkit-kde-authentication-agent-1')
+
+-- Set keyboard layout
+spawn('setxkbmap no -variant nodeadkeys')
